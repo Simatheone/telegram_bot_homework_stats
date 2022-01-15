b6effa9edbb14bd32171b8c6a5913cdd50fa63f0
simatheone <samone07@mail.ru>
2022-01-15 21:31:19 +0300

    Добавил файл с кастомными исключениями

---
+++ exceptions.py
@@ -0,0 +1,10 @@
+class EndpointError(Exception):
+    """Исключение для некорректного Endpoint."""
+
+    pass
+
+
+class InvalidResponse(Exception):
+    """Исключение для некорректного Response."""
+
+    pass
