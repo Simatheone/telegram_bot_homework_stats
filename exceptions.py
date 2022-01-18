71d842b5fd2ccb3c2416a4b9c67cf9aeeb238a97
simatheone <samone07@mail.ru>
2022-01-18 07:13:49 +0300

    Добавил два исключения: 'SendMessageError', 'EmptyHomeworksDict'

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,18 @@
 class EndpointError(Exception):
     """Исключение для некорректного Endpoint."""
-
     pass
 
 
 class InvalidResponse(Exception):
     """Исключение для некорректного Response."""
+    pass
 
+
+class SendMessageError(Exception):
+    """Исключение вызываемое при сбое отправки сообщения в Telegram."""
+    pass
+
+
+class EmptyHomeworksDict(Exception):
+    """Исключение для отсутствующего списка домашних работ."""
     pass
